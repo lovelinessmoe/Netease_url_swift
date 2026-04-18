302cf280f2931093f6fdf061bf92c07953b2bc7d
loveliness <csm@ashes.vip>
2026-04-18 17:55:07 +0800

    Initial Commit

---
+++ netease_music_downloader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  netease_music_downloader
+//
+//  Created by loveliness on 2026/4/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ netease_music_downloader/netease_music_downloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  netease_music_downloaderApp.swift
+//  netease_music_downloader
+//
+//  Created by loveliness on 2026/4/18.
+//
+
+import SwiftUI
+
+@main
+struct netease_music_downloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
